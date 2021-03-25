ec6c7865686a7204481ff40e470c12cd5aa91486
Николай Лыспак <niko.lyspak16@yandex.ru>
2021-03-25 21:50:32 +0700

    Second lesson (not finally)

---
+++ CSharpBasics/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace CSharpBasics
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// Целочисленные значения
+			int a = 5;
+			Console.WriteLine(a);
+			a = 3 + 5;
+			Console.WriteLine(a);
+			int b = a;
+			Console.WriteLine(b);
+
+			int sum = a + b;
+			int dif = a - b;
+			int mul = a * b;
+			var div = a / (double) b;
+
+			int f = a % b;
+
+			Console.WriteLine("Сумма " + sum);
+			Console.WriteLine("Разность " + dif);
+			Console.WriteLine("Произведение " + mul);
+			Console.WriteLine("Частное " + div);
+			Console.WriteLine("Остаток " + f);
+
+			a++; // a = a + 1;
+			Console.WriteLine(a);
+			b--; // b = b - 1;
+			Console.WriteLine(b);
+			a += 3; // a + 3;
+			Console.WriteLine("a: " + a);
+			b -= 2; // b - 2;
+			Console.WriteLine("b: " + b);
+			// Строковые значения
+			string hello = "Hello";
+			Console.WriteLine(hello);
+			string userName = "Usser";
+			Console.WriteLine(userName);
+
+			// Так делать нельзя.
+			//string имяПользователя = "Пользователь";
+			//Console.WriteLine(имяПользователя);
+
+			// Вещественные числа
+			double d = 3, dd = 1;
+			Console.WriteLine(d);
+
+			char charasterValue = 'a';
+			Console.WriteLine(charasterValue);
+
+			// Булевское значение
+			bool ba = true;
+			Console.WriteLine(ba);
+			bool bb = false;
+			Console.WriteLine(bb);
+
+			// Объектный тип
+			object someObject = 5;
+			Console.WriteLine(someObject);
+			string intStringValue = a.ToString();
+
+			var intVar = 5;
+			var doubleVal = 5.0;
+			float floatVal = 5.0f;
+			var stringVar = "Name";
+
+			double anotherDouble = 5;
+
+			int x = 5;
+			string s = "Hello";
+			object o = x;
+			int y = (int)o;
+			object o2 = o;
+			double z = 6.0;
+			Console.WriteLine(o);
+			Console.WriteLine(o2);
+		}
+	}
+}
---
+++ Methods/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace Methods
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			StartProcess();
+			int count = 3;
+			StartAnotherProcess(count);
+			Console.WriteLine(count);
+			StartAnotherProcess(2, "Обучение");
+			StartAnotherProcess(name: "Практика", count: 1);
+
+			StartProcessIn(count);
+			Console.WriteLine($"Счёт до запуска процесса: {count}");
+			StartProcessWithModificators(ref count, out string name);
+			Console.WriteLine($"Счёт: {count}, Название: {name}");
+			int a = 3;
+			int b = 5;
+			int result = MakeAddition(a, b);
+			Console.WriteLine("{0} + {1} = {2} ", a, b, result);
+			Console.WriteLine($"{a} + {b} = {result}");
+
+			int num1 = 8;
+			int num2 = 6;
+			CheckCondition(num1, num2);
+			string logicalResult = (num1 > num2 ? "больше" : "меньше или равно");
+			Console.WriteLine($"Число {num2} {logicalResult} числа {num1}");
+		}
+
+		static void CheckCondition(int num1, int num2)
+		{
+			if ((num1 > num2) && (num1 - num2 > 1))
+			{
+				Console.WriteLine($"Число {num1} сильно больше числа {num2}");
+			}
+			else if (num1 > num2)
+			{
+				Console.WriteLine($"Число {num1} > числа {num2}");
+			}
+			
+			else if (num1 == num2)
+			{
+				Console.WriteLine($"Число {num1} равно числу {num2}");
+			}
+			else if (num1 < num2 || num1 == 6) 
+			{
+				Console.WriteLine("Специальное условие");
+			}
+			else
+			{
+				Console.WriteLine($"Число {num2} > числа {num1}");
+			}
+		}
+
+		static public void StartProcess()
+		{
+			Console.WriteLine("Вызван метод без параметров");
+			return;
+		}
+
+		static void StartAnotherProcess(int count, string name = "Процесс")
+		{
+			count--;
+			Console.WriteLine($"Процесс	запущен {name}, счёт {count}");
+		}
+
+		static void StartProcessIn(in int count)
+		{
+			int count2 = count;
+			count2--;
+			Console.WriteLine($"Параметр передан по ссылке {count}, count2: {count2}");
+		}
+
+		static bool StartProcessWithModificators(ref int count, out string name)
+		{
+			count--;
+
+			name = "Другой процесс";
+
+			return count > 0;
+		}
+
+		static string StartProc(out bool res)
+		{
+			res = false;
+			return "";
+		}
+
+		static private int MakeAddition(int num1, int num2)
+		{
+			int sum = num1 + num2;
+			return sum;
+		}
+	}
+}
